02ff8ceee7919f7013a692765e192dc636c1a7de
Andrew S <andrewsheamls@gmail.com>
2022-02-17 20:39:52 -0700

    created a bunch of variables

---
+++ script.js
@@ -0,0 +1,34 @@
+//function that displays current day at the top 
+
+//write function that saves input to local storage
+
+//write function that GETS input from local storage
+
+//write a function that will display input data on to the screen
+
+//write a function that will change colors based on time
+
+var hourOneInput = $("#hourOneInput");
+var hourTwoInput = $("#hourTwoInput");
+var hourThreeInput = $("#hourThreeInput");
+var hourFourInput = $("#hourFourInput");
+var hourFiveInput = $("#hourFiveInput");
+var hourSixInput = $("#hourSixInput");
+var hourSevenInput = $("#hourSevenInput");
+var hourEightInput = $("#hourEightInput");
+var hourNineInput = $("#hourNineInput");
+var addButton = $(".addButton");
+
+
+//if this button is clicked we need to save the input and display it to the console
+
+addButton.on("click", function() {
+    localStorage.setItem("input", hourOneInput);
+    console.log(input)
+
+})
+
+
+
+
+
